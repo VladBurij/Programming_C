d39ab8289a40078c25f191d0587d52868eab824a
Буров Владислав <burovvladislav4@gmail.com>
2025-02-15 14:39:10 +1000

    Add files via upload

---
+++ Lab1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	double a = 1.2, b = 6.7, c = 4;
+	double d, x1, x2;
+	d = pow(b,2) - 4*a*c;
+	printf("d = %lf \n", d);
+	if (d < 0) printf("No Xs");
+	if (d == 0) {
+		x1 = -b/(2*a);
+		printf("x = %lf \n", x1);
+	}
+	if (d > 0) {
+		x1 = -b + sqrt(d)/(2*a);
+		x2 = -b - sqrt(d)/(2*a);
+		printf("x1 = %lf \n", x1);
+		printf("x2 = %lf \n", x2);
+	}
+	return 0;
+}
