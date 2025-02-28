f88eb10d03726c038e385912056375956fec4e72
Буров Владислав <burovvladislav4@gmail.com>
2025-02-28 18:52:48 +1000

    Add files via upload

--- Lab1/main.c
+++ Lab1/main.c
@@ -6,8 +6,8 @@
 
 int main(int argc, char *argv[]) {
 	double a, b, c, d, x1, x2;
-	char answ[1];
-	do {
+	int answ = 1;
+	while (answ == 1) {
 		printf("Vvedite a: ");
 		scanf("%lf", &a);
 		printf("Vvedite b: ");
@@ -17,18 +17,18 @@ int main(int argc, char *argv[]) {
 		d = pow(b,2) - 4*a*c;
 		printf("d = %lf \n", d);
 		if (d < 0) printf("No Xs \n");
-		if (d == 0) {
+		else if (d == 0) {
 			x1 = -b/(2*a);
 			printf("x = %lf \n", x1);
 		}
-		if (d > 0) {
-			x1 = -b + sqrt(d)/(2*a);
-			x2 = -b - sqrt(d)/(2*a);
+		else {
+			x1 = (-b + sqrt(d))/(2*a);
+			x2 = (-b - sqrt(d))/(2*a);
 			printf("x1 = %lf \n", x1);
 			printf("x2 = %lf \n", x2);
 		}
-		printf("Reset? y or n: ");
-		scanf("%s ", &answ);
-	} while (answ == "y");
+		printf("Reset? 1 or 0: ");
+		scanf("%d ", &answ);
+	}
 	return 0;
 }
