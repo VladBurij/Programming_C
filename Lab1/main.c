4b508d17d56b953551e70cbe288942b174276f72
Буров Владислав <burovvladislav4@gmail.com>
2025-03-14 18:46:05 +1000

    Update main.c

--- Lab1/main.c
+++ Lab1/main.c
@@ -13,19 +13,19 @@ main() {
 		printf("Vvedite c: ");
 		scanf("%f", &c);
 		d = pow(b,2) - 4*a*c;
-		printf("d = %f \n", d);
+		printf("d = %.2f \n", d);
 		if (d < 0) printf("No Xs \n");
 		else if (d == 0) {
 			x1 = -b/(2*a);
-			printf("x = %f \n", x1);
+			printf("x = %.2f \n", x1);
 		}
 		else {
 			x1 = (-b + sqrt(d))/(2*a);
 			x2 = (-b - sqrt(d))/(2*a);
-			printf("x1 = %f \n", x1);
-			printf("x2 = %f \n", x2);
+			printf("x1 = %.2f \n", x1);
+			printf("x2 = %.2f \n", x2);
 		}
 		printf("Reset? 1 or 0: ");
 		scanf("%d ", &answ);
-	};
+	}
 }
