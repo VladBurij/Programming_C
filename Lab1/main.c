dd5d3ad496a845737acaf21817fbc4e0a4123c87
Буров Владислав <burovvladislav4@gmail.com>
2025-03-14 19:34:30 +1000

    Update main.c

--- Lab1/main.c
+++ Lab1/main.c
@@ -3,27 +3,27 @@
 #include <math.h>
 
 main() {
-	float a, b, c, d, x1, x2;
+	double a, b, c, d, x1, x2;
 	int answ = 1;
 	while (answ == 1) {
 		printf("Vvedite a: ");
-		scanf("%f", &a);
+		scanf("%lf", &a);
 		printf("Vvedite b: ");
-		scanf("%f", &b);
+		scanf("%lf", &b);
 		printf("Vvedite c: ");
-		scanf("%f", &c);
+		scanf("%lf", &c);
 		d = pow(b,2) - 4*a*c;
-		printf("d = %.2f \n", d);
+		printf("d = %.2lf \n", d);
 		if (d < 0) printf("No Xs \n");
 		else if (d == 0) {
 			x1 = -b/(2*a);
-			printf("x = %.2f \n", x1);
+			printf("x = %.2lf \n", x1);
 		}
 		else {
 			x1 = (-b + sqrt(d))/(2*a);
 			x2 = (-b - sqrt(d))/(2*a);
-			printf("x1 = %.2f \n", x1);
-			printf("x2 = %.2f \n", x2);
+			printf("x1 = %.2lf \n", x1);
+			printf("x2 = %.2lf \n", x2);
 		}
 		printf("Reset? 1 or 0: ");
 		scanf("%d ", &answ);
