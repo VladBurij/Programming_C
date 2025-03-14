8e5cdc7d6cc93f723938abd71ad2001ac84c09e1
Буров Владислав <burovvladislav4@gmail.com>
2025-03-14 18:37:00 +1000

    Update main.c

--- Lab1/main.c
+++ Lab1/main.c
@@ -2,33 +2,30 @@
 #include <stdlib.h>
 #include <math.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	double a, b, c, d, x1, x2;
+main() {
+	float a, b, c, d, x1, x2;
 	int answ = 1;
 	while (answ == 1) {
 		printf("Vvedite a: ");
-		scanf("%lf", &a);
+		scanf("%f", &a);
 		printf("Vvedite b: ");
-		scanf("%lf", &b);
+		scanf("%f", &b);
 		printf("Vvedite c: ");
-		scanf("%lf", &c);
+		scanf("%f", &c);
 		d = pow(b,2) - 4*a*c;
-		printf("d = %lf \n", d);
+		printf("d = %f \n", d);
 		if (d < 0) printf("No Xs \n");
 		else if (d == 0) {
 			x1 = -b/(2*a);
-			printf("x = %lf \n", x1);
+			printf("x = %f \n", x1);
 		}
 		else {
 			x1 = (-b + sqrt(d))/(2*a);
 			x2 = (-b - sqrt(d))/(2*a);
-			printf("x1 = %lf \n", x1);
-			printf("x2 = %lf \n", x2);
+			printf("x1 = %f \n", x1);
+			printf("x2 = %f \n", x2);
 		}
 		printf("Reset? 1 or 0: ");
 		scanf("%d ", &answ);
-	}
-	return 0;
+	};
 }
