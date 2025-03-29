33ae485d26aa5f069680c581b17ff7599719c7a1
Буров Владислав <burovvladislav4@gmail.com>
2025-03-29 14:10:39 +1000

    Update funcs.h

--- Lab4/funcs.h
+++ Lab4/funcs.h
@@ -1,25 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+int trige_yes (double a, double b, double c) {
+	if ((a*a + b*b == c*c) | (c*c + a*a == b*b) | (c*c + b*b == a*a)) return 1;
+	else return 0;
+}
 
 double trige_perimetr(double a, double b, double c) {
-	if ((a*a + b*b == c*c) | (c*c + a*a == b*b) | (c*c + b*b == a*a)) {
-		double p;
-		p = a + b + c;
-		return p;
-	}
-	else {
-		return -1;
-	}
+	double p;
+	p = a + b + c;
+	return p;
 }
 
 double trige_ploschad(double a, double b, double c) {
-	if ((a*a + b*b == c*c) | (c*c + a*a == b*b) | (c*c + b*b == a*a)) {
-		double s;
-		s = sqrt((p/2) * (p/2 - a) * (p/2 - b) * (p/2 - c));
-		return s;
+	double p;
+	p = a + b + c;
+	double s;
+	s = sqrt((p/2) * (p/2 - a) * (p/2 - b) * (p/2 - c));
+	return s;
+}
+
+double trige_heigth(double a, double b, double c, int i) {
+	double p, s, h;
+	p = a + b + c;
+	s = sqrt((p/2) * (p/2 - a) * (p/2 - b) * (p/2 - c));
+	switch(i) {
+		case 1:
+			h = (2*s)/a;
+		case 2:
+			h = (2*s)/b;
+		case 3:
+			h = (2*s)/c;
+		default: 
+			return 0;
 	}
-	else {
-		return -1;
-	}
+	return h;
 }
