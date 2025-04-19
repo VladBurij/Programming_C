f64cefea3410563be294724606c0c44d9d5833f7
Буров Владислав <burovvladislav4@gmail.com>
2025-04-19 14:09:26 +1000

    Update main.c

--- Lab7/main.c
+++ Lab7/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct human {
 	char name[20], surname[20];
@@ -11,18 +12,25 @@ int sorting(const void* a, const void* b) {
 }
 
 main() {
-	//FILE *input = fopen("C:/Users/Vlad/Desktop/prog_C/input.txt", "r");
-	//FILE *output = fopen("C:/Users/Vlad/Desktop/prog_C/output.txt", "w");
-	struct human humans[4];
-	for (int i = 0; i < 4; i++) {
-		printf("Input: surname name birthyear: ");
-		scanf("%s %s %d", &humans[i].surname, &humans[i].name , &humans[i].year);
-	}
-	qsort(humans, 4, sizeof(struct human), sorting);
-	for (int i = 0; i < 4; i++) printf("%s %s %d\n", humans[i].surname, humans[i].name , humans[i].year);
-	//while ((fscanf(input, "%s %s %d\n", &surname, &name, &year))!=EOF) {
-		//fprintf(output, "%s %s %d\n", surname, name, year);
+	int i = 0, k;
+	char name[20], surname[20];
+	int year;
+	FILE *input = fopen("input.txt", "r");
+	FILE *output = fopen("output.txt", "w");
+	struct human humans[100];
+	//for (i = 0; i < 4; i++) {
+		//printf("Input: surname name birthyear: ");
+		//scanf("%s %s %d", &humans[i].surname, &humans[i].name , &humans[i].year);
 	//}
-	//fclose(input);
-	//fclose(output);
+	//for (int i = 0; i < 4; i++) printf("%s %s %d\n", humans[i].surname, humans[i].name , humans[i].year);
+	while ((fscanf(input, "%s %s %d\n", surname, name, &year))!=EOF) {
+		strcpy(humans[i].surname, surname);
+		strcpy(humans[i].name, name);
+		humans[i].year = year;
+		printf("%s %s %d\n", humans[i].surname, humans[i].name, humans[i].year);
+		
+		i++;
+	}
+	qsort(humans, i, sizeof(struct human), sorting);
+	for (k = 0; k < i; k++) fprintf(output, "%s %s %d\n", humans[k].surname, humans[k].name, humans[k].year);
 }
