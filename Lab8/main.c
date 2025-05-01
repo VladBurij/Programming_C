80e1fef350c7d29c9b920419d183024f149773b3
Буров Владислав <burovvladislav4@gmail.com>
2025-05-01 17:13:08 +1000

    Update main.c

--- Lab8/main.c
+++ Lab8/main.c
@@ -8,27 +8,120 @@ struct human {
 	double height;
 };
 
+char answ[2];
+
 int sorting_surname(const void* a, const void* b) {
-	return strcmp(((struct human*)a) -> surname, ((struct human*)b) -> surname);
-}
+	if (strcmp(((struct human*)a) -> surname, ((struct human*)b) -> surname) == 0) {
+		switch (answ[1]) {
+			case '2':
+				return strcmp(((struct human*)a) -> name, ((struct human*)b) -> name);
+				break;
+			case '3':
+				return ((struct human*)a) -> year - ((struct human*)b) -> year;
+				break;
+			case '4':
+				return strcmp(((struct human*)a) -> sex, ((struct human*)a) -> sex);
+				break;
+			case '5':
+				return ((struct human*)a) -> height - ((struct human*)b) -> height;
+				break;
+			default:
+				break;
+		}
+	}
+	else return strcmp(((struct human*)a) -> surname, ((struct human*)b) -> surname);
+};
 int sorting_name(const void* a, const void* b) {
-	return strcmp(((struct human*)a) -> name, ((struct human*)b) -> name);
+	if (strcmp(((struct human*)a) -> name, ((struct human*)b) -> name) == 0) {
+		switch (answ[1]) {
+			case '1':
+				return strcmp(((struct human*)a) -> surname, ((struct human*)b) -> surname);
+				break;
+			case '3':
+				return ((struct human*)a) -> year - ((struct human*)b) -> year;
+				break;
+			case '4':
+				return strcmp(((struct human*)a) -> sex, ((struct human*)a) -> sex);
+				break;
+			case '5':
+				return ((struct human*)a) -> height - ((struct human*)b) -> height;
+				break;
+			default:
+				break;
+		}
+	}
+	else return strcmp(((struct human*)a) -> name, ((struct human*)b) -> name);
 }
 int sorting_year(const void* a, const void* b) {
+	if (((struct human*)a) -> year == ((struct human*)b) -> year) {
+		switch (answ[1]) {
+			case '1':
+				return strcmp(((struct human*)a) -> surname, ((struct human*)b) -> surname);
+				break;
+			case '2':
+				return strcmp(((struct human*)a) -> name, ((struct human*)b) -> name);
+				break;
+			case '4':
+				return strcmp(((struct human*)a) -> sex, ((struct human*)a) -> sex);
+				break;
+			case '5':
+				return ((struct human*)a) -> height - ((struct human*)b) -> height;
+				break;
+			default:
+				break;
+		}
+	}
+	else
 	return ((struct human*)a) -> year - ((struct human*)b) -> year;
 }
 int sorting_sex(const void* a, const void* b) {
-	return strcmp(((struct human*)a) -> sex, ((struct human*)a) -> sex);
+	if (strcmp(((struct human*)a) -> sex, ((struct human*)b) -> sex) == 0) {
+		switch (answ[1]) {
+			case '1':
+				return strcmp(((struct human*)a) -> surname, ((struct human*)b) -> surname);
+				break;
+			case '2':
+				return strcmp(((struct human*)a) -> name, ((struct human*)b) -> name);
+				break;
+			case '3':
+				return ((struct human*)a) -> year - ((struct human*)b) -> year;
+				break;
+			case '5':
+				return ((struct human*)a) -> height - ((struct human*)b) -> height;
+				break;
+			default:
+				break;
+		}
+	}
+	else return strcmp(((struct human*)a) -> sex, ((struct human*)a) -> sex);
 }
 int sorting_height(const void* a, const void* b) {
-	return ((struct human*)a) -> height - ((struct human*)b) -> height;
+	if (((struct human*)a) -> height == ((struct human*)b) -> height) {
+		switch (answ[1]) {
+			case '1':
+				return strcmp(((struct human*)a) -> surname, ((struct human*)b) -> surname);
+				break;
+			case '2':
+				return strcmp(((struct human*)a) -> name, ((struct human*)b) -> name);
+				break;
+			case '3':
+				return ((struct human*)a) -> year - ((struct human*)b) -> year;
+				break;
+			case '4':
+				return strcmp(((struct human*)a) -> sex, ((struct human*)a) -> sex);
+				break;
+			default:
+				break;
+		}
+	}
+	else return ((struct human*)a) -> height - ((struct human*)b) -> height;
 }
 
 main() {
 	FILE *input = fopen("C:/Users/Vlad/Desktop/prog_C/input.txt", "r");
 	FILE *output = fopen("C:/Users/Vlad/Desktop/prog_C/output.txt", "w");
 	struct human humans[100], s_humans[100];
-	int i = 0, k, answ;
+	int i = 0, k;
 	char ans = 'y';
 	while ((fscanf(input, "%s %s %s %d %lf\n", humans[i].surname, humans[i].name, humans[i].sex, &humans[i].year, &humans[i].height))!=EOF) {
 		s_humans[i] = humans[i];
@@ -37,21 +130,21 @@ main() {
 	for (k = 0; k < i; k++) printf("%s %s %d %s %.2lf\n", s_humans[k].surname, s_humans[k].name, s_humans[k].year, s_humans[k].sex, s_humans[k].height);
 	while (ans == 'y'){
 		printf("Choose sorting: 1) Surname; 2) Name; 3) Birthyear; 4) Sex; 5) Height\n");
-		scanf("%d", &answ);
-		switch (answ){
-			case 1:
+		scanf("%s", &answ);
+		switch (answ[0]){
+			case '1':
 				qsort(s_humans, i, sizeof(struct human), sorting_surname);
 				break;
-			case 2:
+			case '2':
 				qsort(s_humans, i, sizeof(struct human), sorting_name);
 				break;
-			case 3:
+			case '3':
 				qsort(s_humans, i, sizeof(struct human), sorting_year);
 				break;
-			case 4:
+			case '4':
 				qsort(s_humans, i, sizeof(struct human), sorting_sex);
 				break;
-			case 5:
+			case '5':
 				qsort(s_humans, i, sizeof(struct human), sorting_height);
 				break;
 			default:
